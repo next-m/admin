ce736b9d59e6f301b17f7081e6455ca7e2802aea
HoungJinNam_spolive <houngjinnam_spolive@HoungJinNam-spoliveui-MacBookPro.local>
2022-12-19 16:39:20 +0900

    dashboard

--- src/views/main.vue
+++ src/views/main.vue
@@ -7,47 +7,23 @@
     <div class="dividing">
       <section class="member-manage">
         <!-- 컨테이너 하단에 갈 경우 이 섹션과 같은 level에서 만들어야 한다. -->
-        <!--section class="member-list section-box">
+        <section class="member-list section-box">
           <div class="square-container">
-            <square titled="전체매출" :total="totalTodal" :month="totalMonth" :day="totalDay"></square>
-            <square titled="광고" :total="adverTodal" :month="adverMonth" :day="adverDay"></square>
-            <square titled="광고 영상" :total="videoDay" :month="videoMonth" :day="videoDay"></square>
+            <square titled="씨앗적립" :total="totalTodal" :month="totalMonth" :day="totalDay"></square>
+            <square titled="씨앗후원" :total="donationTodal" :month="donationMonth" :day="donationDay"></square>
           </div>
-          <div class="store-traffic-container mt10"></div>
+          <!--div class="store-traffic-container mt10"></div-->
         </section>
-      </section-->
-      <!--section class="righs">
+      </section>
+      <section class="righs">
         <div class="d-flex mt10" style="height: 135px">
-          <rectangle :naming="dataName" titled="ADDTUNE 회원" status="member" :info="userInfoArr" class="mr10"></rectangle>
-          <rectangle :naming="dataName2" titled="ADDTUNE 스토어" :info="storeArr"></rectangle>
+          <rectangle :naming="dataName" titled="회원현황" status="member" :info="userInfoArr" class="mr10"></rectangle>
+          <rectangle :naming="dataName2" titled="크리에이터 등록" :info="creatorArr" class="mr10"></rectangle>          
+          <rectangle :naming="dataName3" titled="콘탠츠 등록" :info="contentsArr"></rectangle>
         </div>
-        <div class="store-traffic-container mt10">
-          <h2>스토어 트래픽 센서 상태 <span>(Store Traffice Sensor Status)</span></h2>
+        <!--div class="store-traffic-container mt10">
+          <h2>1:1 문의 <span></span></h2>
           <div class="store-traffic-wrap">
-            <div class="num-wrap">
-              <ul>
-                <li @click="view('total')" :class="stat === 'total' ? 'active' : ''">
-                  <div class="subject">전체</div>
-                  <div class="data">{{ sensorObj.total }}</div>
-                </li>
-                <li @click="view('active')" :class="stat === 'active' ? 'active' : ''">
-                  <div class="subject">정상</div>
-                  <div class="data">{{ sensorObj.active }}</div>
-                </li>
-                <li @click="view('down')" :class="stat === 'down' ? 'active' : ''">
-                  <div class="subject">다운</div>
-                  <div class="data" :class="stat === 'down' ? 'activing' : 'warning'">{{ sensorObj.down }}</div>
-                </li>
-                <li @click="view('warning')" :class="stat === 'warning' ? 'active' : ''">
-                  <div class="subject">경고</div>
-                  <div class="data" :class="stat === 'warning' ? 'activing' : 'warning'">{{ sensorObj.warning }}</div>
-                </li>
-                <li @click="view('prog')" :class="stat === 'prog' ? 'active' : ''">
-                  <div class="subject">준비중</div>
-                  <div class="data">{{ sensorObj.in_progress }}</div>
-                </li>
-              </ul>
-            </div>
             <div class="tbl-wrap">
               <table class="tbl user-list-tbl">
                 <colgroup>
@@ -57,9 +33,9 @@
                 </colgroup>
                 <thead>
                   <tr>
-                    <th>센서코드</th>
-                    <th class="left">스토어명</th>
-                    <th class="right pr10">센서수</th>
+                    <th>고유코드</th>
+                    <th class="left">제목</th>
+                    <th class="right pr10">답변여부</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -77,7 +53,7 @@
               </table>
             </div>
           </div>
-        </div>
+        </div-->
         <!-- 알럿 -->
         <alim :open="snackbar" :txt="text" :color="color" @reset="emitReset"></alim>
       </section>
@@ -107,15 +83,17 @@ export default {
       content: "",
       time: "",
       dataName: ["회원가입수", "휴먼/탈퇴 (누적)", "DAY", "휴면계정", "누적", "탈퇴 계정"],
-      dataName2: ["전체", "운영 준비중", "운영", "운영종료", "노출불가", "운영보류"],
+      dataName2: ["전체", "신청", "승인", "종료"],
+      dataName3: ["전체", "승인대기", "승인", "승인불가"],
       userInfoArr: ["", "", 0, 0, 0, 0],
-      storeArr: [0, 0, 0, 0, 0, 0],
+      creatorArr: [0, 0, 0, 0, "", ""],
+      contentsArr: [0, 0, 0, 0, "", ""],
       totalTodal: 0,
       totalMonth: 0,
       totalDay: 0,
-      adverTodal: 0,
-      adverMonth: 0,
-      adverDay: 0,
+      donationTodal: 0,
+      donationMonth: 0,
+      donationDay: 0,
       videoTodal: 0,
       videoMonth: 0,
       videoDay: 0,
@@ -130,14 +108,14 @@ export default {
     ...mapGetters("menu", ["dashBoard"]),
   },
   watch: {
-    // timeVal(val) {
-    //   if (Math.floor(val) > 100) {
-    //     this.getDashBoard();
-    //   }
-    // },
+    timeVal(val) {
+      if (Math.floor(val) > 100) {
+        this.getDashBoard();
+      }
+    },
   },
   mounted() {
-    //this.getDashBoard();
+    this.getDashBoard();
     this.getDay();
     setInterval(this.getDay.bind(this), 1000);
     setInterval(this.timeIncrease.bind(this), 1000);
@@ -185,30 +163,42 @@ export default {
         bus.$emit("start:spinner");
         await this.$store.dispatch("menu/DASHBOARD");
         const res       = this.dashBoard.nextmApiResult.dashboard;
-        this.totalTodal = res.buyInfo.total.total;
-        this.totalMonth = res.buyInfo.total.month;
-        this.totalDay   = res.buyInfo.total.day;
-        this.adverTodal = res.buyInfo.adver.total;
-        this.adverMonth = res.buyInfo.adver.month;
-        this.adverDay   = res.buyInfo.adver.day;
-        this.videoTodal = res.buyInfo.video.total;
-        this.videoMonth = res.buyInfo.video.month;
-        this.videoDay   = res.buyInfo.video.day;
-        this.sensorObj  = res.wiInfo.count;
-        this.sensor     = res.wiInfo.list;
-        const user      = res.userInfo;
-        const store     = res.storeInfo;
-        this.userInfoArr = ["", "", user.dayUser, user.dormancyUser, user.totalUser, user.withdrawalUser];
-        this.stat       = "total";
-        this.sensorList = [...this.sensor.active, ...this.sensor.down, ...this.sensor.warning, ...this.sensor.in_progress];
-        this.storeArr   = [
-          store.companyStoretotal,
-          store.companyStoreAfter,
-          store.companyStoreStart,
-          store.companyStoreEnd,
-          store.companyStoreErr,
-          store.companyStoreHold,
-        ];
+
+         this.totalTodal = res.seedInfo.total.total;
+         this.totalMonth = res.seedInfo.total.month;
+         this.totalDay   = res.seedInfo.total.day;
+         this.donationTodal = res.seedInfo.donation.total;
+         this.donationMonth = res.seedInfo.donation.month;
+         this.donationDay   = res.seedInfo.donation.day;
+
+         const user      = res.userInfo;         
+         this.userInfoArr = ["", "", user.dayUser, user.dormancyUser, user.totalUser, user.withdrawalUser];
+
+         const creator      = res.creatorInfo;         
+         this.creatorArr = [creator.creatorTotal, creator.creatorAfter, creator.creatorStart, creator.creatorEnd, "", ""];         
+
+         const content      = res.contentInfo;         
+         this.contentsArr = [content.total, content.before, content.approval, content.sorry, "", ""];         
+
+
+        // this.videoTodal = res.buyInfo.video.total;
+        // this.videoMonth = res.buyInfo.video.month;
+        // this.videoDay   = res.buyInfo.video.day;
+        // this.sensorObj  = res.wiInfo.count;
+        // this.sensor     = res.wiInfo.list;
+
+        // const store     = res.storeInfo;
+
+        // this.stat       = "total";
+        // this.sensorList = [...this.sensor.active, ...this.sensor.down, ...this.sensor.warning, ...this.sensor.in_progress];
+        // this.storeArr   = [
+        //   store.companyStoretotal,
+        //   store.companyStoreAfter,
+        //   store.companyStoreStart,
+        //   store.companyStoreEnd,
+        //   store.companyStoreErr,
+        //   store.companyStoreHold,
+        // ];
         this.timeVal = 0;
       } catch (error) {
         this.alim(error, this.errorColor);
@@ -252,6 +242,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.square{
+  width:50%;
+}
 .main-container {
   padding: 40px 20px;
   max-width: 100% !important;
@@ -349,10 +342,10 @@ export default {
   display: flex;
   justify-content: space-between;
   & .member-manage {
-    width: 60% !important;
+    width: 40% !important;
   }
   & .righs {
-    width: calc(40% - 10px);
+    width: calc(60% - 10px);
   }
 }
 .time-line {
